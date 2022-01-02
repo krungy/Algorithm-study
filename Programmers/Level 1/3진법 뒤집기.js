cb7c657ac65b5d19cec9368ab48d1a9c0a83272f
krungy <furler711@gmail.com>
2022-01-02 16:34:10 +0900

    Lv1. 3진법 뒤집기

---
+++ Programmers/Level 1/3진법 뒤집기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  var answer = 0;
+  let third = n.toString(3);
+  let reverse = "";
+
+  for (let i = third.length - 1; i >= 0; i--) {
+    reverse += third[i];
+  }
+  answer = parseInt(reverse * 1, 3);
+  return answer;
+}
