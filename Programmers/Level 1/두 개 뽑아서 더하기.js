8dccee24f55a30a91efc8352a3727edcfe42f451
krungy <furler711@gmail.com>
2022-01-03 16:25:00 +0900

    Lv1. 두 개 뽑아서 더하기

---
+++ Programmers/Level 1/두 개 뽑아서 더하기.js
@@ -0,0 +1,28 @@
+function solution(numbers) {
+  var answer = [];
+  let tmp = 0;
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let bool = 1;
+      let sum = numbers[i] + numbers[j];
+      for (let k = 0; k < answer.length; k++) {
+        if (answer[k] == sum) bool = 0;
+      }
+      if (bool == 1) answer.push(sum);
+    }
+  }
+
+  for (let i = 0; i < answer.length; i++) {
+    for (let j = 0; j < answer.length - i; j++) {
+      if (answer[j] > answer[j + 1]) {
+        let tmp = answer[j + 1];
+        answer[j + 1] = answer[j];
+        answer[j] = tmp;
+      }
+    }
+  }
+  return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/68644
