f6337b8b27b1ed1ce0cdc60bb36a126df375abcc
krungy <furler711@gmail.com>
2022-01-09 16:53:52 +0900

    Lv1. 서울에서 김서방 찾기

---
+++ Programmers/Level 1/서울에서 김서방 찾기.js
@@ -0,0 +1,7 @@
+function solution(seoul) {
+  let indexKim = seoul.indexOf("Kim");
+  let answer = `김서방은 ${indexKim}에 있다`;
+  return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12919
