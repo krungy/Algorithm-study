b1a116ca5ffdcbc185119df242ca1f16c86e4026
krungy <furler711@gmail.com>
2022-01-10 14:40:39 +0900

    Lv1. 같은 숫자는 싫어

---
+++ Programmers/Level 1/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+  var answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    answer[answer.length - 1] !== arr[i] && answer.push(arr[i]);
+  }
+  return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12906
