9337d636faf23d1757fe550e54d368f79b5768f8
krungy <furler711@gmail.com>
2022-02-05 16:16:15 +0900

    Lv2. N개의 최소공배수

---
+++ Programmers/Level 2/N개의 최소공배수.js
@@ -0,0 +1,21 @@
+const gdc = (a, b) => {
+  while (b > 0) {
+    let tmp = b;
+    b = a % b;
+    a = tmp;
+  }
+  return a;
+};
+
+const lcm = (a, b) => {
+  return (a * b) / gdc(a, b);
+};
+
+function solution(arr) {
+  var answer = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    answer = lcm(answer, arr[i]);
+  }
+  return answer;
+}
